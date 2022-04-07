3ea9be479e16aafc4bcaf702c002709970d5f93f
Joao Talone Gouveia <jjtucki4@gmail.com>
2022-04-07 20:46:29 +0100

    Added const.h

---
+++ const.h
@@ -0,0 +1,21 @@
+/*Definir constantes*/
+#define MAX_NUM_AEROPORTOS 40   /* número máximo de areoportos */
+#define MAX_NUM_VOOS 30000  /* número máximo de voos */
+#define MAX_CODIGO_AEROPORTO 4  /* dimensão do código do aeroporto */
+#define MAX_NOME_PAIS 31    /* dimensão do nome do pais */
+#define MAX_NOME_CIDADE 51  /* dimensão do nome da cidade */
+
+#define MAX_CODIGO_VOO 7    /* dimensão do código do voo */
+#define MAX_DATA 11     /* dimensão da data */
+#define MAX_HORA 6      /* dimensão da hora */
+
+#define NAO_EXISTE -1       /* código de erro */
+
+#define ANO_INICIO 2022     /* ano inicial do sistema */
+#define DIAS_ANO 365        /* número de dias no ano */
+#define HORAS_DIA 24        /* número de horas por dia */
+#define MINUTOS_HORA 60     /* número de minutos numa hora */
+#define MINUTOS_DIA 1440    /* número de minutos do dia */
+
+#define TRUE 1          /* verdadeiro */
+#define FALSE 0         /* falso */
